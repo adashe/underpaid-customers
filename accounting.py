e88b2d017d4da4938364c36b481f1d9329957efd
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-02-06 05:12:41 +0000

    Correct payments function"

--- accounting.py
+++ accounting.py
@@ -24,38 +24,63 @@ customer6_name = "Ashley"
 customer6_melons = 3
 customer6_paid = 2.00
 
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
+# customer1_expected = customer1_melons * melon_cost
+# if customer1_expected != customer1_paid:
+#     print(f"{customer1_name} paid ${customer1_paid:.2f},",
+#           f"expected ${customer1_expected:.2f}"
+#           )
+
+# customer2_expected = customer2_melons * melon_cost
+# if customer2_expected != customer2_paid:
+#     print(f"{customer2_name} paid ${customer2_paid:.2f},",
+#           f"expected ${customer2_expected:.2f}"
+#           )
+
+# customer3_expected = customer3_melons * melon_cost
+# if customer3_expected != customer3_paid:
+#     print(f"{customer3_name} paid ${customer3_paid:.2f},",
+#           f"expected ${customer3_expected:.2f}"
+#           )
+
+# customer4_expected = customer4_melons * melon_cost
+# if customer4_expected != customer4_paid:
+#     print(f"{customer4_name} paid ${customer4_paid:.2f},",
+#           f"expected ${customer4_expected:.2f}"
+#           )
+
+# customer5_expected = customer5_melons * melon_cost
+# if customer5_expected != customer5_paid:
+#     print(f"{customer5_name} paid ${customer5_paid:.2f},",
+#           f"expected ${customer5_expected:.2f}"
+#           )
+
+# customer6_expected = customer6_melons * melon_cost
+# if customer6_expected != customer6_paid:
+#     print(f"{customer6_name} paid ${customer6_paid:.2f},",
+#           f"expected ${customer6_expected:.2f}"
+#           )
+
+# Creates a function that compares customer payments to expepcted payments
+
+def correct_payments(customer_name, customer_melons, customer_paid):
+
+    # Calculates expected cost
+
+    customer_expected = customer_melons * melon_cost
+
+    # Compares expected cost to actual amount paid
+
+    if customer_expected != customer_paid:
+
+        # Prints out string indicating if a customer over or underpaid
+        
+        print(f"{customer_name} paid ${customer_paid:.2f},",
+          f"expected ${customer_expected:.2f}"
           )
 
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
+correct_payments(customer1_name, customer1_melons, customer1_paid)
+correct_payments(customer2_name, customer2_melons, customer2_paid)
+correct_payments(customer3_name, customer3_melons, customer3_paid)
+correct_payments(customer4_name, customer4_melons, customer4_paid)
+correct_payments(customer5_name, customer5_melons, customer5_paid)
+correct_payments(customer6_name, customer6_melons, customer6_paid)
